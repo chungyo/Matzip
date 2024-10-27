caad53f0514e77f0632110669bfc08a84f11a0bf
chungyo <kimchungy98@gmail.com>
2024-10-27 23:50:39 +0900

    feat: 로그인 기능 완성
    - React-query 사용
    - useAuth 훅 생성

---
+++ src/hooks/queries/useAuth.ts
@@ -0,0 +1,99 @@
+import { QueryKey, useMutation, useQuery, UseQueryOptions } from "@tanstack/react-query";
+import { getAccessToken, getProfile, logout, postLogin, postSignup } from "../../api/auth";
+import { UseMutationCustomOptions, UseQueryCustomOptions } from "../../types/common";
+import { removeEncryptStorage, setEncryptStorage } from "../../utils";
+import axiosInstance from "../../api/axios";
+import { removeHeader, setHeader } from "../../utils/header";
+import { useEffect } from "react";
+import queryClient from "../../api/queryClient";
+
+
+//v5
+function useSignup(mutationOptions?: Omit<UseMutationCustomOptions, "mutationFn">) {
+  return useMutation({
+    mutationFn: postSignup,
+    ...mutationOptions
+  });
+}
+
+function useLogin(mutationOptions?: UseMutationCustomOptions) {
+  return useMutation({
+    mutationFn: postLogin,
+    onSuccess: ({ accessToken, refreshToken }) => {
+      setEncryptStorage("refreshToken", refreshToken);
+      setHeader("Authorization", `Bearer $ {accessToken}`);
+      axiosInstance.defaults.headers.common["Authorization"] = accessToken;
+    },
+    onSettled: () => {
+      queryClient.refetchQueries({queryKey: ["auth", "getAccessToken"]});
+      queryClient.invalidateQueries({queryKey: ["auth", "getProfile"]});
+    },
+    ...mutationOptions
+  });
+}
+
+function useGetRefreshToken() {
+  const { isSuccess, data, isError } = useQuery({
+    queryKey: ["auth", "getAccessToken"],
+    queryFn: getAccessToken,
+    staleTime: 1000 * 60 * 30 - 1000 * 60 * 3,
+    refetchInterval: 1000 * 60 * 30 - 1000 * 60 * 3,
+    refetchOnReconnect: true,
+    refetchIntervalInBackground: true
+  });
+
+  useEffect(() => {
+    if(isSuccess) {
+      setHeader('Authorization', `Bearer $ {data.accessToken}`);
+      setEncryptStorage('refreshToken', data.refreshToken);
+    }
+  }, [isSuccess]);
+
+  useEffect(() => {
+    if(isError) {
+      removeHeader('Authorization');
+      removeEncryptStorage('refreshToken');
+    }
+  }, [isError]);
+
+  return { isSuccess, isError };
+}
+
+
+
+function useGetProfile(queryOptions?: UseQueryCustomOptions) {
+  return useQuery({
+    queryKey: ["auth", "getProfile"],
+    queryFn: getProfile,
+    ...queryOptions,
+  });
+}
+
+function useLogout(mutationOptions?: UseMutationCustomOptions){
+  return useMutation({
+    mutationFn: logout,
+    onSuccess: () => {
+      removeHeader('Authorization');
+      removeEncryptStorage('refreshToken');
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({queryKey: ["auth"]});
+    },
+    ...mutationOptions,
+  });
+}
+
+function useAuth() {
+  const signupMutation = useSignup();
+  const refreshTokenQuery = useGetRefreshToken();
+  const getProfileQuery = useGetProfile({
+    enabled: refreshTokenQuery.isSuccess,
+  });
+  const isLogin = getProfileQuery.isSuccess;
+  const loginMutation = useLogin();
+  const logoutMutation = useLogout();
+
+  return { signupMutation, loginMutation, isLogin, getProfileQuery, logoutMutation};
+}
+
+export default useAuth;
